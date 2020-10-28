eb1b4458b9ace38eee9877ade3b67032eb5b40a8
Nemo <liuzhexian11235@gmail.com>
2020-10-28 13:54:59 -0700

    Update Vec.java

--- Vec.java
+++ Vec.java
@@ -1,22 +1,38 @@
 import java.lang.*;
+import java.util.*;
 
 public class Vec {
 
 	public double x, y, z;
+	Scanner scan = new Scanner(System.in);
+	
+	// default constructor that will asks user input for a new vector
+	public Vec() {
+		System.out.print("x: ");
+		x = scan.nextDouble();
+		System.out.print("y: ");
+		y = scan.nextDouble();
+		System.out.print("z: ");
+		z = scan.nextDouble();
+	}
 	
 	// constructor
-	public Vec(int x, int y, int z) {
+	public Vec(double x, double y, double z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 	
 	// this function resets the values of a vector
-	public void setVec(int x, int y, int z) {
+	public void setVec(double x, double y, double z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
+	
+	// return the vector as a string
+	public String toString() 
+	{	return "(" + x + ", " + y + ", " + z + ")";	}
 	
 	// this function calculates cross product of 2 vectors and return the result as a new vector.
 	public static Vec crossProduct(Vec v1, Vec v2) {
@@ -26,6 +42,10 @@ public class Vec {
 		result.z = v1.x * v2.y - v1.y * v2.x;
 		return result;
 	}
+	
+	// dot product of 2 vectors
+	public Double dotProduct(Vec v2) 
+	{	return x * v2.x + y * v2.y + z * v2.z;	}
 	
 	
 	// this function normalize a vector.
